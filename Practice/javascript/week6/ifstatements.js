ff09ec568306501e5aa9448524a2b6bf621629ed
Nastassia Dudley <nastassiaserina@gmail.com>
2020-02-27 13:04:56 -0800

    if statements

---
+++ Practice/javascript/week6/ifstatements.js
@@ -0,0 +1,90 @@
+// If-Else Statements, AKA conditional statements
+// If (condition){}
+
+if(true) {
+
+    console.log("This code ran if the if statement was true.");
+
+}
+
+console.log("End of script");
+
+
+// Comparison operators or "Comparitors"
+
+/*
+== equal to
+>= greater than or equal
+> greater than
+<= lesser than or equal
+< lesser than
+!= not equal (should not equal to be true)
+*/
+
+
+
+/*
+// var age = 22;
+
+if (age >= 21) {
+    console.log("Congrats, go get drunk!");
+}
+
+var password = "123456";
+
+if ("123456" == "123456") [
+console.log("welcome to your profile!"); */
+
+// Not true
+
+
+
+
+var age = 22;
+
+if (age >= 21) {
+    console.log ("Congrats, go get drunk!");
+}
+
+var password = "Abcdef";
+
+if ("Abcdef" == "abcdef") [
+console.log("welcome to your profile!");
+
+// False
+
+
+
+
+
+/*
+var age = 22;
+
+if (age >= 21) {
+    console.log("Congrats, go get drunk!");
+}
+
+var password = "A";
+
+if ("A" >= "a") [
+console.log("welcome to your profile!");
+
+*/
+
+// False
+
+
+
+
+
+
+var isSleepy = false;
+
+if (isSleepy == false) 
+
+
+
+var something = false;
+
+if (something == false);
+console.log("go home and sleep");
