bb84f9934b27729c85c8f7ddbafe01d9a5b3e92e
Nastassia Dudley <nastassiaserina@gmail.com>
2020-03-01 17:38:29 -0800

    Project pages

--- Practice/javascript/week6/loops.js
+++ Practice/javascript/week6/loops.js
@@ -1,1 +1,72 @@
 console.log("Testing");
+
+/*
+var number = 1;
+
+while (false) do {
+    console.log(number);
+
+}
+*/
+
+
+
+/*
+var number = 1;
+
+while (number < 100) {
+    console.log(number);
+
+    number = number + 1;
+
+};
+*/
+
+
+/*
+var number = 1;
+
+while (number < 100000) do {
+    console.log(number);
+
+    number = number + 1;
+
+};
+*/
+
+
+
+
+
+// While loop "while (condition) {code}"
+
+// A loop needs a counter so we can track how many times it has loop.
+
+var counter = 0;
+
+
+// If counter is less tha 10, then...
+while (counter < 10) {
+    //consol.log(number);
+
+    // ...console.log hello, and add a 1 to the counter.
+    console.log("hello");
+    counter = counter + 1;
+
+};
+
+
+
+
+// Do while loop " do {} while ();"
+
+var counter2 = 0;
+
+
+//do the code first.
+do {
+    console.log("goodbye");
+    counter2 = counter2 + 1;
+
+    // And then check to see if we do it again....
+} while (false);
