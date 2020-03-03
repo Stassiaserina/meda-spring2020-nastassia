390cdb39de748f9892b19603afad432349ba4a64
Nastassia Dudley <nastassiaserina@gmail.com>
2020-03-03 10:28:24 -0800

    practice.js Arrays and i

---
+++ Practice/javascript/week7/practice.js
@@ -0,0 +1,82 @@
+// property
+Math.PI;
+Math.SQRT2;
+
+//Method
+Math.random();
+
+function random() {
+
+}
+
+
+//for (var i = 0; i < 10; i++) {
+
+}//
+
+
+var myArray = [null, undefined NaN, 0, "" false];
+
+Console.Console(myArray[3]);
+
+
+myArray[2] = 100;
+
+
+console.log(myArray);
+6
+myArray[6] = {};
+
+console.log(myArray);
+
+myArray[0] = null;
+myArray[1] = null;
+myArray[2] = null;
+
+// TODO: convert this into a loop, to empty all existing items.
+
+myArray.push("hello there!");
+
+console.log(myArray);
+
+// Array pop method. No argument, removes and RETURNS the last item in the array.
+
+var removedDataPiece = myArray.pop();
+
+console.log(myArray);console.log(removedDataPiece);
+
+// Array shift method. no argument, removes and RETURNS the first item in the array.
+
+var removedDataPiece = myArray.shift();
+
+console.log(myArray);
+console.log(removedFirstItem);
+
+// Array unshift method, argument provided is added to the start of the array.
+
+myArray.unshift(1000);
+console.log(myArray);
+
+// Array length property. Returns the amount of items in the array in the form of a Number.
+
+console.log( myArray.length );
+
+
+
+// Looping through each item of an array and console logging its value.
+
+/* for (var i = 0; i < 10; i++) {
+
+    console.log(myArray[1]);
+
+}
+*/
+
+
+
+
+for (var i = 0; i < myArray.length; i++) {
+
+    console.log(myArray{i});
+
+}
