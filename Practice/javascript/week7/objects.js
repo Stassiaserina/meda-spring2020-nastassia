94efdd70b8c4ce5181d90d89796c80968bf6dc47
Nastassia Dudley <nastassiaserina@gmail.com>
2020-03-03 12:46:43 -0800

    Started on Objects

---
+++ Practice/javascript/week7/objects.js
@@ -0,0 +1,22 @@
+var employeeSalaries = [10000, 20000, 30000];
+
+console.log(employeeSalaries[1]);
+
+// var myFirstObject = {jesse: 10000, mark: 20000, sarah: 30000};   OR
+var myFirstObject = {"jesse": 10000, "mark": 20000, "sarah": 30000};
+
+cons.log(myFirstObject.Jesse);
+Math.PI
+
+
+var person = {
+    "first_name": "jesse",
+    "last_name": "saint",
+    "age": 29,
+    "address": "200 Happy St. Amazing City, Nebraska 23889",
+    "fav_color": "blue",
+    "salary": 56000,
+    "phone": 4840039829
+}
+
+console.log(person.last_name);
