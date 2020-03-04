0af18295f226914cf2af007515f5f4cce814a1cd
Nastassia Dudley <nastassiaserina@gmail.com>
2020-03-04 12:59:59 -0800

    pyramid builder complete

--- Practice/javascript/week7/pyramidbuilder.js
+++ Practice/javascript/week7/pyramidbuilder.js
@@ -4,8 +4,9 @@ console.log("***");
 console.log("**");
 console.log("*");
 
-
-var pyramidRow = "*";
+                                       // Delimiters
+/*
+var pyramidRow = "";
 
 console.log(pyramidRow);
 
@@ -15,10 +16,147 @@ console.log(pyramidRow);
 
 
 
-
-for (var i = 0: i < 10; i++) {
+for (var i = 0; i < 10; i++) {
 
     pyramidRow = pyramidRow + "*";
 
     console.log(pyramidRow);
 }
+*/
+
+
+// var pyramidArray =  pyramidRow.split("");
+
+
+/*
+pyramidArray.pop();
+
+pyramidRow = pyramidArray.join("");
+
+console.log(pyramidRow);
+
+
+
+pyramidArray.pop();
+
+pyramidRow = pyramidArray.join("");
+
+console.log(pyramidRow);
+
+
+
+pyramidArray.pop();
+
+pyramidRow = pyramidArray.join("");
+
+console.log(pyramidRow);
+
+*/
+
+
+
+
+for (var i = 0; i < pyramidHeight; i++) {
+
+    pyramidRow = pyramidRow * "*";
+
+    console.log(pyramidRow);
+}
+
+
+
+/* Coppied / replaced into the below function pyramidBuilder(pyramidHeight) {}
+
+var randomNumber = (Math.random() * 20) + 1;
+randomNumber = Math.floor(randomNumber);
+
+
+
+var pyramidRow = "";
+var pyramidHeight = randomNumber;
+
+
+for (var i = 0; i < pyramidHeight; i++) {
+
+    pyramidRow = pyramidRow + "*";
+
+    console.log(pyramidRow);
+}
+
+
+
+
+var pyramidArray =  pyramidRow.split("");
+
+for (var i = 0; i < 4; i++) {
+
+    pyramidArray.pop();
+
+    pyramidRow = pyramidArray.join("");
+
+    console.log(pyramidRow);
+}
+
+*/
+
+
+function pyramidBuilder(pyramidHeight) {  
+
+    var randomNumber = (Math.random() * 20) + 1;
+    randomNumber = Math.floor(randomNumber);
+
+
+    if (height == undefined) {
+        var randomNumber = (Math.random() * 20) + 1;
+        pyramidHeight = Math.floor(randomNumber);
+    } else {
+        pyramidHeight = height;
+    }
+
+
+
+    var pyramidRow = "";
+
+
+    var pyramidHeight = height;
+
+
+    for (var i = 0; i < pyramidHeight; i++) {
+
+    pyramidRow = pyramidRow + "*";
+
+    console.log(pyramidRow);
+}
+
+
+
+
+var pyramidArray =  pyramidRow.split("");
+
+for (var i = 0; i < 4; i++) {
+
+    pyramidArray.pop();
+
+    pyramidRow = pyramidArray.join("");
+
+    console.log(pyramidRow);
+}
+
+
+}
+
+pyramidBuilder(3);
+pyramidBuilder();
+pyramidBuilder(3);
+
+
+for (var i = 0; i < 100; i++) {
+    pyramidBuilder();
+}
+
+
+
+
+
+
+
